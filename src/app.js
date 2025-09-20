5a13c8ef73fb84b0722b26b5adfabcbae714f315
Gurukiran Bhat <gbhat@eg.dk>
2025-09-20 18:21:49 +0530

    explore get, post and delete

--- src/app.js
+++ src/app.js
@@ -7,8 +7,40 @@ const app = express();
 //     res.send("Hello from the server!");
 // })
 
+// app.use("/hello/2", (req, res) => {
+//     res.send("Hello from the /hello/2 endpoint!");
+// })
+
+// app.use("/hello", (req, res) => {
+//     res.send("Hello from the /hello endpoint!");
+// })
+
+// app.use("/test", (req, res) => {
+//     res.send("Hello from the /test endpoint!");
+// })
+
+// app.use("/", (req, res) => {
+//     res.send("Hello from the / endpoint!");
+// })
+
+/* <--------------------------------------------------------> */
+
+// This will match all the HTTP methods (GET, POST, PUT, DELETE, etc.) to /test
 app.use("/test", (req, res) => {
     res.send("Hello from the /test endpoint!");
+})
+
+// This will match only GET requests to /user
+app.get("/user", (req, res) => {
+    res.send({firstName: 'Gurukiran', lastName: 'Bhat'});
+})
+
+app.post("/user", (req, res) => {
+    res.send('Data has been saved successfully!');
+})
+
+app.delete("/user", (req, res) => {
+    res.send('Data has been deleted successfully!');
 })
 
 app.listen(6969, () => {
