10b648d44ff549477e870311af1cbf388b199ef9
Gurukiran-bhat29 <gurukiranbhat@gmail.com>
2025-10-08 21:05:25 +0530

    Added feed API

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,9 @@ const express = require("express");
 const { userAuth } = require("../middleware/auth");
 const userRouter = express.Router();
 const ConnectionRequest = require('../models/connectionRequest')
+const User = require("../models/user");
+
+const USER_SAFE_DATA = ['firstName', 'lastName', 'emailId', 'photoUrl', 'about'];
 
 // Get all the pending connection request for the loggedIn user
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
@@ -32,8 +35,8 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
         { fromUserId: loggedInUser._id, status: 'accepted'},
       ]
     })
-    .populate("fromUserId", ['firstName', 'lastName'])
-    .populate("toUserId", ['firstName', 'lastName']);
+    .populate("fromUserId", USER_SAFE_DATA)
+    .populate("toUserId", USER_SAFE_DATA);
 
     const data = connectionRequests.map(row => {
       if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
@@ -44,6 +47,49 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
 
     res.json({ data });
 
+  } catch (err) {
+    res.status(400).send('ERROR: ' + err.message)
+  }
+})
+
+userRouter.get('/feed', userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit; // Max limit is 50
+
+    const skip = (page - 1) * limit;
+
+    // Find all the connection request (sent + received) of the loggedIn user
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [
+        { fromUserId: loggedInUser._id},
+        { toUserId: loggedInUser._id},
+      ]
+    })
+    .select(['fromUserId', 'toUserId'])
+    // .populate("fromUserId", ['firstName', 'lastName']) NOTE: It can be used if you want to see the user details
+    // .populate("toUserId", ['firstName', 'lastName']);
+
+    const hideUsersFromFeed = new Set();
+    connectionRequests.forEach(req => {
+      hideUsersFromFeed.add(req.fromUserId.toString());
+      hideUsersFromFeed.add(req.toUserId.toString());
+    })
+    
+    const users = await User.find({
+      // This will exclude the loggedIn user and the users with whom the connection request is sent or received
+      // Array.from is used to convert the set into array
+      // $nin -> Not in this array and $ne -> Not equal to this value
+      $and: [
+        { _id: { $ne: loggedInUser._id } },
+        { _id: { $nin: Array.from(hideUsersFromFeed) } }
+      ]
+    }).select(USER_SAFE_DATA).skip(skip).limit(limit);
+
+    res.json({ data: users });
   } catch (err) {
     res.status(400).send('ERROR: ' + err.message)
   }
