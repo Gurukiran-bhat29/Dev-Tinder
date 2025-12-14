bde17b42e7f69c9b11e9e4c5885bb07c8cfd1551
Gurukiran-bhat29 <gurukiranbhat@gmail.com>
2025-12-14 20:53:44 +0530

    feat: Building Real-time live application Chat

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const connectDb = require('./config/database');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const app = express();
+const http = require('http');
+const initializeSocket = require('./utils/socket');
 
 app.use(cors({
   origin: 'http://localhost:5173',
@@ -18,6 +20,7 @@ const profileRouter = require('./routes/profile');
 const requestRouter = require('./routes/request');
 const userRouter = require('./routes/user');
 const paymentRouter = require('./routes/payment');
+const chatRouter = require('./routes/chat');
 
 require('./utils/cronjobs');
 
@@ -26,11 +29,14 @@ app.use('/', profileRouter);
 app.use('/', requestRouter);
 app.use('/', userRouter);
 app.use('/', paymentRouter); // Add this line
+app.use('/', chatRouter);
 
+const server = http.createServer(app);
+initializeSocket(server);
 
 connectDb().then(() => {
   try {
-    app.listen(7070, () => {
+    server.listen(7070, () => {
       console.log('Server is running on port 7070');
     })
   } catch (err) {
